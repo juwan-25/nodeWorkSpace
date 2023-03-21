367c2cbb4eb98e67c23900091673a4d598bcc0b8
Juwan Lee <ljw05w5@gmail.com>
2023-03-21 18:58:24 +0900

    백틱 활용하기

---
+++ 230321/test01.js
@@ -0,0 +1,13 @@
+//백틱 사용
+let pi = 3.14159265;
+let radius = 10; //반지름
+
+console.log("원의 둘레 : "+(2*pi*radius));
+console.log("원의 넓이 : "+(pi*radius*radius));
+
+
+console.log(`원의 둘레 : (2*${pi*radius})`);
+console.log(`원의 넓이 : (${radius*pi*radius})`);
+
+console.log(`반지름 : ${radius})`);
+console.log("반지름 : "+radius);
